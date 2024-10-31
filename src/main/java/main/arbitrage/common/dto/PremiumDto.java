b6ed69bb4364e9629dca20880a491225ec711e81
dlgusgh4608 <dlgusgh4608@gmail.com>
2024-10-31 17:08:18 +0900

    feat: premium dto

---
+++ src/main/java/main/arbitrage/common/dto/PremiumDto.java
@@ -0,0 +1,18 @@
+package main.arbitrage.common.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Builder
+@Getter
+public class PremiumDto {
+    private String symbol;
+    private BigDecimal premium;
+    private BigDecimal domestic;
+    private BigDecimal overseas;
+    private double usdToKrw;
+    private Long domesticTradeAt;
+    private Long overseasTradeAt;
+}
