0445a866945c85d8d8536bf801e93f1243782a38
dlgusgh4608 <dlgusgh4608@gmail.com>
2024-12-12 13:03:04 +0900

    fix: new exchange rate is not working in domain

--- src/main/java/main/arbitrage/domain/exchangeRate/service/ExchangeRateService.java
+++ src/main/java/main/arbitrage/domain/exchangeRate/service/ExchangeRateService.java
@@ -1,6 +1,7 @@
 package main.arbitrage.domain.exchangeRate.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import main.arbitrage.domain.exchangeRate.dto.ExchangeRateDto;
 import main.arbitrage.domain.exchangeRate.entity.ExchangeRate;
 import main.arbitrage.domain.exchangeRate.repository.ExchangeRateRepository;
@@ -11,23 +12,32 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ExchangeRateService {
     private final ExchangeRateRepository exchangeRateRepository;
     private final Map<String, ExchangeRateDto> prevExchangeRateMap = new ConcurrentHashMap<>();
 
     public ExchangeRate setExchangeRate(ExchangeRateDto exchangeRateDto) {
         double rate = exchangeRateDto.getRate();
+
         if (rate == 0) return null;
 
         String key = exchangeRateDto.getFromCurrency() + "_" + exchangeRateDto.getToCurrency();
 
         ExchangeRateDto prevDto = prevExchangeRateMap.get(key);
-        if (prevDto == null) return null;
 
-        double prevDtoRate = prevDto.getRate();
-        if (prevDtoRate == rate) return null;
+        if (prevDto == null) {
+            log.info("New exchange rate [{}]: {}", key, rate);
 
-        prevExchangeRateMap.put(key, exchangeRateDto);
+            prevExchangeRateMap.put(key, exchangeRateDto);
+        } else {
+            double prevDtoRate = prevDto.getRate();
+            if (prevDtoRate == rate) return null;
+
+            log.info("Update exchange rate [{}]: {} -> {}", key, prevDtoRate, rate);
+
+            prevExchangeRateMap.put(key, exchangeRateDto);
+        }
 
         return exchangeRateRepository.save(ExchangeRateDto.toEntity(exchangeRateDto));
     }
