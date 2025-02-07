30da6f2f46b290cbf8768541f768b3db1edf5aca
dlgusgh4608 <dlgusgh4608@gmail.com>
2025-02-07 14:29:12 +0900

    ci: test

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 APP_DIR="/home/ubuntu/app"
 JAR="$APP_DIR/arbitrageJava.jar"
-LOG="/home/ubuntu/log/stop_process.log"
+LOG="/home/ubuntu/log/applicationStart.log"
 
 NOW=$(date '+%Y-%m-%d %T')
 
-
-echo "[$NOW] stop_server process start" >> $LOG
+echo "[$NOW] start applicationStart process" >> $LOG
 if [ -f $JAR ]; then
   pkill -f "java -jar $JAR" || echo "[$NOW] Process not running" >> $LOG
 else
   echo "[$NOW] Error: JAR file not found at $JAR"
   exit 1
 fi
-echo "[$NOW] stop_server process end" >> $LOG
+
+nohup java -jar $JAR &
+
+echo "[$NOW] end applicationStart process" >> $LOG
--- scripts/create-dir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-APP_DIR="/home/ubuntu/app"
-LOG_DIR="/home/ubuntu/log"
-
-
-mkdir -p $APP_DIR
-mkdir -p $LOG_DIR
