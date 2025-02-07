3a4859c9f229d4fdffe0b64d951378d7f34f7eec
dlgusgh4608 <dlgusgh4608@gmail.com>
2025-02-07 10:16:23 +0900

    ci: test

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 APP_DIR="/home/ubuntu/app"
-JAR_NAME="arbitrage-java.jar"
-
-echo "Starting application..."
-if [ -f "$APP_DIR/$JAR_NAME" ]; then
-  nohup java -jar "$APP_DIR/$JAR_NAME" &
-else
-  echo "Error: JAR file not found at $APP_DIR/$JAR_NAME"
-  exit 1
-fi
+JAR_NAME="arbitrageJava.jar"
--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 APP_DIR="/home/ubuntu/app"
-JAR_NAME="arbitrage-java.jar"
-
-echo "Stopping application..."
-if [ -f "$APP_DIR/$JAR_NAME" ]; then
-  pkill -f "java -jar $APP_DIR/$JAR_NAME" || echo "Process not running"
-else
-  echo "Error: JAR file not found at $APP_DIR/$JAR_NAME"
-  exit 1
-fi
+JAR_NAME="arbitrageJava.jar"
